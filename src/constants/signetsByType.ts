9bd39143c24409928214a798c1561759aa384e98
Hawkie <ahribestplayer@gmail.com>
2023-06-17 22:07:52 +0300

    Add signets for Helix, Daybreak, Stars, Reverie

--- src/constants/signetsByType.ts
+++ src/constants/signetsByType.ts
@@ -295,8 +295,177 @@ export const SIGNETS_BY_TYPE: SignetsByType = {
 			],
 		},
 	],
-	[SignetTypes.Helix]: [],
-	[SignetTypes.Daybreak]: [],
-	[SignetTypes.Stars]: [],
-	[SignetTypes.Reverie]: [],
+	[SignetTypes.Helix]: [
+		{ name: 'Act I: Magic', rarity: SignetRarity.Normal },
+		{ name: 'Act II: Pendulum', rarity: SignetRarity.Normal },
+		{ name: 'Act III: Conflict', rarity: SignetRarity.Normal },
+		{ name: 'Act IV: Spoon', rarity: SignetRarity.Normal },
+		{ name: 'Act V: Spider Web', rarity: SignetRarity.Normal },
+		{ name: 'Act VI: Sneer', rarity: SignetRarity.Normal },
+		{
+			name: 'Interlude: Reversed Spiral',
+			rarity: SignetRarity.Core,
+			signets: [
+				{
+					name: 'Act VII: Half-Closed Door',
+					rarity: SignetRarity.Enchanced,
+				},
+				{
+					name: 'Act VIII: Adorned Chamber',
+					rarity: SignetRarity.Enchanced,
+				},
+				{
+					name: 'Finale: Broken Truth',
+					rarity: SignetRarity.Enchanced,
+				},
+			],
+		},
+		{
+			name: 'Intermezzo: Holistic Helix',
+			rarity: SignetRarity.Core,
+			signets: [
+				{
+					name: 'Puppetry: Interlacing Cells',
+					rarity: SignetRarity.Enchanced,
+				},
+				{ name: 'Drama: Origin of Slumber', rarity: SignetRarity.Enchanced },
+				{
+					name: 'Saga: Overlapping Needles',
+					rarity: SignetRarity.Enchanced,
+				},
+			],
+		},
+	],
+	[SignetTypes.Daybreak]: [
+		{ name: 'Talons that Desecrate the Santuary', rarity: SignetRarity.Normal },
+		{ name: 'Wings that Mask the Sanguine Moon', rarity: SignetRarity.Normal },
+		{ name: 'Horns that Rip the Bleak Sky', rarity: SignetRarity.Normal },
+		{
+			name: 'Eyes that Overlook the Vicious Abyss',
+			rarity: SignetRarity.Normal,
+		},
+		{ name: 'Breath that Slanders Sulphur', rarity: SignetRarity.Normal },
+		{ name: 'Heart that Committed Seven Crimes', rarity: SignetRarity.Normal },
+		{
+			name: 'Daybreak, an Undying Light',
+			rarity: SignetRarity.Core,
+			signets: [
+				{
+					name: 'Unbending Hero',
+					rarity: SignetRarity.Enchanced,
+				},
+				{
+					name: 'Undetermined Judgement',
+					rarity: SignetRarity.Enchanced,
+				},
+				{
+					name: 'Unwavering Promise',
+					rarity: SignetRarity.Enchanced,
+				},
+			],
+		},
+		{
+			name: 'Daybreak, Lone Beacon',
+			rarity: SignetRarity.Core,
+			signets: [
+				{
+					name: 'Dawn. Perpetual Wait',
+					rarity: SignetRarity.Enchanced,
+				},
+				{ name: 'Choice, Ensuing Regret', rarity: SignetRarity.Enchanced },
+				{
+					name: 'Heart, Unshakable Character',
+					rarity: SignetRarity.Enchanced,
+				},
+			],
+		},
+	],
+	[SignetTypes.Stars]: [
+		{ name: 'Red & Hot', rarity: SignetRarity.Normal },
+		{ name: 'Blue & Cold', rarity: SignetRarity.Normal },
+		{ name: 'Yellow & Warm', rarity: SignetRarity.Normal },
+		{
+			name: 'Black & Dark',
+			rarity: SignetRarity.Normal,
+		},
+		{ name: 'White & Bright', rarity: SignetRarity.Normal },
+		{ name: 'Grey & Empty', rarity: SignetRarity.Normal },
+		{
+			name: 'Shining Like Stars',
+			rarity: SignetRarity.Core,
+			signets: [
+				{
+					name: 'Blooming Like Flowers',
+					rarity: SignetRarity.Enchanced,
+				},
+				{
+					name: 'Burning Like Flames',
+					rarity: SignetRarity.Enchanced,
+				},
+				{
+					name: 'Stretching Like Leaves',
+					rarity: SignetRarity.Enchanced,
+				},
+			],
+		},
+		{
+			name: 'Faded Stars',
+			rarity: SignetRarity.Core,
+			signets: [
+				{
+					name: 'Petals in the Wind',
+					rarity: SignetRarity.Enchanced,
+				},
+				{ name: 'Fallen Leaves', rarity: SignetRarity.Enchanced },
+				{
+					name: 'A Flame Extinguished',
+					rarity: SignetRarity.Enchanced,
+				},
+			],
+		},
+	],
+	[SignetTypes.Reverie]: [
+		{ name: 'Motto of Meow', rarity: SignetRarity.Normal },
+		{ name: 'Back-Curled Tail', rarity: SignetRarity.Normal },
+		{ name: 'Neighborhood Chatters', rarity: SignetRarity.Normal },
+		{
+			name: 'Flexible P&C',
+			rarity: SignetRarity.Normal,
+		},
+		{ name: `Merchant's Philosophy`, rarity: SignetRarity.Normal },
+		{ name: 'Shiny & Valuable', rarity: SignetRarity.Normal },
+		{
+			name: 'Empty Dreams, Self, Set, and Joy',
+			rarity: SignetRarity.Core,
+			signets: [
+				{
+					name: 'Improvised Drama',
+					rarity: SignetRarity.Enchanced,
+				},
+				{
+					name: 'Setsuna Paw',
+					rarity: SignetRarity.Enchanced,
+				},
+				{
+					name: 'This Is Not Where We Die',
+					rarity: SignetRarity.Enchanced,
+				},
+			],
+		},
+		{
+			name: 'Empty Illusions, Empty Fantasies',
+			rarity: SignetRarity.Core,
+			signets: [
+				{
+					name: 'Improv Magic: Zeroing',
+					rarity: SignetRarity.Enchanced,
+				},
+				{ name: 'Snowy Paw', rarity: SignetRarity.Enchanced },
+				{
+					name: 'My Dreams Will Come True',
+					rarity: SignetRarity.Enchanced,
+				},
+			],
+		},
+	],
 };
